b62399d3b301751efeaee38cc4892d13af6722f6
KOREADEVELOPERS <sulabhsharma3624@gmail.com>
2025-11-14 11:18:09 +0530

    Update controller.java

--- src/main/java/Backend_Bugg/Debugg/controller.java
+++ src/main/java/Backend_Bugg/Debugg/controller.java
@@ -1,6 +1,8 @@
 package Backend_Bugg.Debugg;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.*;
+import java.util.Map;
 
 @RestController
 @RequestMapping("/api/code")
@@ -10,8 +12,11 @@ public class controller {
     @Autowired
     private services codeService;
 
+    // ✅ Updated endpoint to accept JSON { code, input }
     @PostMapping("/run")
-    public attributes runCode(@RequestBody String code) {
-        return codeService.compileAndRun(code);
+    public attributes runCode(@RequestBody Map<String, String> body) {
+        String code = body.get("code");
+        String input = body.getOrDefault("input", ""); // If input is empty, default to empty string
+        return codeService.compileAndRunWithInput(code, input);
     }
 }
